b94184ce4a8044edb08fc5777b98525e9a83c00a
asprtam <44498326+asprtam@users.noreply.github.com>
2019-10-24 23:11:43 +0200

    Updates

---
+++ precache-manifest.b8607608275fc30e3d418d9e347dbfec.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ef8f1027e4910422cf171c1f26c6c71",
+    "url": "/react_sun_gradient/index.html"
+  },
+  {
+    "revision": "2eb96ce9d09b542aeb2b",
+    "url": "/react_sun_gradient/static/css/main.e93c9f58.chunk.css"
+  },
+  {
+    "revision": "b8ccaca75b878473fa8b",
+    "url": "/react_sun_gradient/static/js/2.5f4c69f4.chunk.js"
+  },
+  {
+    "revision": "2eb96ce9d09b542aeb2b",
+    "url": "/react_sun_gradient/static/js/main.ffaa5ad9.chunk.js"
+  },
+  {
+    "revision": "724ebc4f3bb3980dc1e7",
+    "url": "/react_sun_gradient/static/js/runtime-main.72a5124d.js"
+  }
+]);
